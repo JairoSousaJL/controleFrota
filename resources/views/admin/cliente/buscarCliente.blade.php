6b47ee3887c8064353436d3abcf98eb51225cecd
JAIRO SOUSA <jairo111sousa@live.com>
2021-08-16 15:23:27 -0300

    icone

--- resources/views/admin/cliente/buscarCliente.blade.php
+++ resources/views/admin/cliente/buscarCliente.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pesquisar Clientes</title>
+    <link rel="icon"  type="image/png"  href="{{asset('images/icon.png')}}">
     <link rel="stylesheet" href="{{asset('frota/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('frota/css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('frota/css/style.css')}}">
